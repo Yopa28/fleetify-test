afdbed664f4f52a2f31166c0ef518c869d700a9b
Yopa28 <sandyboangmanalu@gmail.com>
2026-05-19 18:15:23 +0700

    feat(backend): implement main application entrypoint with fiber server

---
+++ backend/cmd/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+
+	"fleetify-backend/database"
+	"fleetify-backend/models"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	database.ConnectDatabase()
+
+	database.DB.AutoMigrate(
+	&models.User{},
+	&models.Vehicle{},
+	&models.MasterItem{},
+	&models.MaintenanceReport{},
+	&models.ReportItem{},
+)
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Fleetify API Running",
+		})
+	})
+
+	log.Fatal(app.Listen(":8080"))
+}
